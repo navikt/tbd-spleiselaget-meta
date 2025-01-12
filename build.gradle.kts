e1d4c3cb2909b3c911e0ca8b5ead5b64f92f7957
david steinsland <david@davidsteinsland.net>
2025-01-12 10:20:18 +0100

    gjør det mulig å laste ned sources-jar via intellij
    i meta-repoet virker det å være sånn at intellij bruker root-build.gradle-filen til å bedømme hvilke repositories den skal forsøke når den laster ned sources.
    må altså legge til github-repository for at vi skal kunne laste ned sources-jar til tbd-libs osv.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,16 @@
 repositories {
+    val githubPassword: String? by project
     mavenCentral()
+    /* ihht. https://github.com/navikt/utvikling/blob/main/docs/teknisk/Konsumere%20biblioteker%20fra%20Github%20Package%20Registry.md
+        så plasseres github-maven-repo (med autentisering) før nav-mirror slik at github actions kan anvende førstnevnte.
+        Det er fordi nav-mirroret kjører i Google Cloud og da ville man ellers fått unødvendige utgifter til datatrafikk mellom Google Cloud og GitHub
+     */
+    maven {
+        url = uri("https://maven.pkg.github.com/navikt/maven-release")
+        credentials {
+            username = "x-access-token"
+            password = githubPassword
+        }
+    }
+    maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
 }
