f377e51af6077454de32a8f5ee5f647b8cf8a8fd
david steinsland <david@davidsteinsland.net>
2025-08-05 08:35:32 +0200

    skriver mindre til console når ting går bra

--- .bin/upgrade_gradle.sh
+++ .bin/upgrade_gradle.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 
 if ! test -f build.gradle.kts; then
   echo "Not a gradle project."
@@ -8,14 +7,25 @@ if ! test -f build.gradle.kts; then
 fi
 
 echo "Upgrading ..."
-./gradlew wrapper --gradle-version=latest --distribution-type bin
-./gradlew wrapper
+./gradlew wrapper --gradle-version=latest --distribution-type bin >/dev/null 2>&1
+./gradlew wrapper >/dev/null 2>&1
+
 echo "Building post-upgrade ..."
-./gradlew -q --warning-mode=all build
+output=$(./gradlew -q --warning-mode=all build 2>&1)
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+  echo "$output"
+  exit $exit_code
+fi
+
 echo "Committing changes ..."
 git add gradle gradlew.bat gradlew
 
 COMMIT_MESSAGE="Bump Gradle wrapper til latest"
 git commit -m"$COMMIT_MESSAGE"
 
-
+if [ $? -ne 0 ]; then
+  echo "No changes to commit."
+else
+  echo "Changes committed successfully."
+fi
