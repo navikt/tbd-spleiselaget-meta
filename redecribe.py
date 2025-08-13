b66d23c3bfc30194d547ee6f28f895170e61c16c
Hakon Arneng Holmstedt <hakon.a.holmstedt@gmail.com>
2025-08-13 15:27:28 +0200

    dingsen som leser meta describe outputter en hel html-side

--- redecribe.py
+++ redecribe.py
@@ -3,7 +3,37 @@ import sys
 last_system = ""
 description = ""
 chunk = []
+pre = '''
+<html>
+    <head>
+        <title>Alle våre apper</title>
+<style>
+    dl {
+        display: grid;
+        grid-gap: 4px 16px;
+        grid-template-columns: max-content;
+    }
+    dt {
+        font-weight: bold;
+    }
+    dd {
+        margin: 0;
+        grid-column-start: 2;
+    }
+    .missing {
+        color: red;
+    }
+</style>
+    </head>
+    <body>
+'''
 
+post = '''
+    </body>
+</html>
+'''
+
+print(pre)
 print("<dl>")
 for line in sys.stdin:
     if len(chunk) != 4:
@@ -11,8 +41,9 @@ for line in sys.stdin:
 
     if len(chunk) == 4:
         if not chunk[2]:
-            print("<dt>" + chunk[1] + "</dt><dd>mangler beskrivelse</dd>")
+            print("<dt>" + chunk[1] + "</dt><dd class='missing'>mangler beskrivelse</dd>")
         else:
             print("<dt>" + chunk[1] + "</dt><dd>" + chunk[2] + "</dd>")
         chunk = []
 print("</dl>")
+print(post)
