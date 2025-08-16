dc521678bcf70b7321d1cd2d325660387e06afac
david steinsland <david@davidsteinsland.net>
2025-08-16 14:01:17 +0200

    oppdaterer repo-listen

--- lag_meta_json.sh
+++ lag_meta_json.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 gh api /teams/$(gh api /orgs/navikt/teams/tbd | jq -r '.id')/repos --paginate \
-  | jq -r '{projects: [.[] | select(.archived == false and (.topics | index("speilvendt") | not) and .name != "helsearbeidsgiver-bro-sykepenger" and .name != "afp" and .name != "tbd-spleiselaget-meta" and .name != "tbd" and .name != "tbd-meta" and .name != "vault-iac" and .name != "tbd-datadeling" and .name != "sparkiv-subsumsjon" and .name != "sparkles") | {"meta-\(.name)":.clone_url}] | add}' \
+  | jq -r '{projects: [.[] | select(.archived == false and (.topics | index("speilvendt") | not) and .name != "helsearbeidsgiver-bro-sykepenger" and .name != "afp" and .name != "tbd-spleiselaget-meta" and .name != "tbd" and .name != "tbd-meta" and .name != "vault-iac" and .name != "tbd-datadeling" and .name != "sparkiv-subsumsjon" and .name != "sparkles" and .name != "helse-spaghet") | {"meta-\(.name)":.clone_url}] | add}' \
   | tee > .meta
 
