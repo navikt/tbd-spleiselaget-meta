21d834a9be71c422aaef9ec25778b3e7ec35bffb
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2026-01-27 15:49:32 +0100

    Fikser script som henter repositories
    Sorterer før vi skriver til fil slik at listen ikke stokker seg om hver gang vi kjører scriptet.
    
    Co-authored-by: Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>
    Co-authored-by: Sivert Lunsæter <sivert.frang.lunseter@nav.no>
    Co-authored-by: Vemund Strom <vemund.strom@nav.no>

--- lag_meta_json.sh
+++ lag_meta_json.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 gh api /teams/$(gh api /orgs/navikt/teams/tbd | jq -r '.id')/repos --paginate \
-  | jq -r '{projects: [.[] | select(.archived == false and (.topics | index("speilvendt") | not) and .name != "helsearbeidsgiver-bro-sykepenger" and .name != "afp" and .name != "tbd-spleiselaget-meta" and .name != "tbd" and .name != "tbd-meta" and .name != "vault-iac" and .name != "tbd-datadeling" and .name != "sparkiv-subsumsjon" and .name != "sparkles" and .name != "helse-spaghet" and .name != "helse-spane") | {"meta-\(.name)":.clone_url}] | add}' \
+  | jq --sort-keys -r '{projects: [.[] | select(.archived == false and (.topics | index("speilvendt") | not) and .name != "helsearbeidsgiver-bro-sykepenger" and .name != "afp" and .name != "tbd-spleiselaget-meta" and .name != "tbd" and .name != "tbd-meta" and .name != "vault-iac" and .name != "tbd-datadeling" and .name != "sparkiv-subsumsjon" and .name != "sparkles" and .name != "helse-spaghet" and .name != "helse-spane") | {"meta-\(.name)":.clone_url}] | add}' \
   | tee > .meta
 
